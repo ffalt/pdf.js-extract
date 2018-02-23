7576df38d45ca90897afdb8043acc33bdc7be06e
ffalt <ffalty@gmail.com>
2018-02-23 21:47:56 +0100

    set verbosity to VerbosityLevel.ERRORS as default instead of VerbosityLevel.WARNINGS, replace node-ensure requirement with local file

--- update_pdfjs.sh
+++ update_pdfjs.sh
@@ -3,10 +3,13 @@ cd pdfjs-temp
 npm install
 npm install gulp
 ./node_modules/.bin/gulp dist-pre
-cp ./build/dist/build/pdf.combined.js ../lib/pdfjs/pdf.combined.js
-cp ./pdfjs.config ../lib/pdfjs/pdfjs.version
+#cp ./build/dist/build/pdf.js ../lib/pdfjs/pdf.js
+#cp ./build/dist/build/pdf.worker.js ../lib/pdfjs/pdf.worker.js
+node -e 'var fs=require("fs");fs.writeFileSync("../lib/pdfjs/pdf.js",fs.readFileSync("./build/dist/build/pdf.js").toString().replace("verbosity = VerbosityLevel.WARNINGS","verbosity = VerbosityLevel.ERRORS").replace("node-ensure","./ensure.js"));process.exit()'
+node -e 'var fs=require("fs");fs.writeFileSync("../lib/pdfjs/pdf.worker.js",fs.readFileSync("./build/dist/build/pdf.worker.js").toString().replace("verbosity = VerbosityLevel.WARNINGS","verbosity = VerbosityLevel.ERRORS"));process.exit()'
+cp ./build/version.json ../lib/pdfjs/version.json
 cp ./LICENSE ../lib/pdfjs/LICENSE
 echo Used version
-tail pdfjs.config
+tail ./build/version.json
 cd ..
 rm -Rf pdfjs-temp
