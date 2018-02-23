30d966b4b47cdeff8633dafa0e376e26ad469e10
ffalt <ffalty@gmail.com>
2018-02-23 21:49:08 +0100

    show example usage of text extract utils

--- example/encrypted.js
+++ example/encrypted.js
@@ -1,8 +1,15 @@
 var fs = require('fs');
 var PDFExtract = require('../lib').PDFExtract;
+
 var pdfExtract = new PDFExtract();
-pdfExtract.extract('./encrypted.pdf', { password: 'password'}, function (err, data) {
+pdfExtract.extract('./encrypted.pdf', {password: 'password'}, function (err, data) {
 	if (err) return console.log(err);
 	fs.writeFileSync('./encrypted-output.json', JSON.stringify(data, null, '\t'));
+	var lines = PDFExtract.utils.pageToLines(data.pages[0], 2);
+	var rows = PDFExtract.utils.extractTextRows(lines);
+	var text = rows.map(function (row) {
+		return row.join('');
+	}).join('\n');
+	fs.writeFileSync('./encrypted-output.txt', text);
 	console.log(JSON.stringify(data, null, '\t'));
 });
--- example/example.js
+++ example/example.js
@@ -1,8 +1,14 @@
 var fs = require('fs');
 var PDFExtract = require('../lib').PDFExtract;
 var pdfExtract = new PDFExtract();
-pdfExtract.extract('./example.pdf', {} /* options, currently nothing available*/, function (err, data) {
+pdfExtract.extract('./example.pdf', {} /* options*/, function (err, data) {
 	if (err) return console.log(err);
 	fs.writeFileSync('./example-output.json', JSON.stringify(data, null, '\t'));
+	var lines = PDFExtract.utils.pageToLines(data.pages[0], 2);
+	var rows = PDFExtract.utils.extractTextRows(lines);
+	var text = rows.map(function (row) {
+		return row.join('');
+	}).join('\n');
+	fs.writeFileSync('./example-output.txt', text);
 	console.log(JSON.stringify(data, null, '\t'));
 });
