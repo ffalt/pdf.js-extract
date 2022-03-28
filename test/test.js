55c7e2d19754cdffd2643d7aa805a36ee0bb374b
ffalt <ffalty@gmail.com>
2022-03-28 10:54:40 +0200

    chore(pdf.js): bump to 2.14.110
    BREAKING CHANGE: metadata obj keys are included directly (no longer in a child object _metadata)

--- test/test.js
+++ test/test.js
@@ -173,20 +173,20 @@ describe("PDFExtract.tools", () => {
 					const content = [
 						"Adobe Acrobat PDF Files",
 						"Adobe® Portable Document Format (PDF) is a universal file format that preserves all",
-						"of  the  fonts,  formatting,  colours  and  graphics  of  any  source  document,  regardless  of",
+						"of the fonts, formatting, colours and graphics of any source document, regardless of",
 						"the application and platform used to create it.",
 						"Adobe PDF is an ideal format for electronic document distribution as it overcomes the",
 						"problems commonly encountered with electronic file sharing.",
 						"• Anyone, anywhere can open a PDF file. All you need is the free Adobe Acrobat",
-						"Reader.  Recipients  of  other  file  formats  sometimes  can't  open  files  because  they",
+						"Reader. Recipients of other file formats sometimes can't open files because they",
 						"don't have the applications used to create the documents.",
 						"• PDF files always print correctly on any printing device.",
-						"• PDF  files  always  display  exactly  as  created,  regardless  of  fonts,  software,  and",
+						"• PDF files always display exactly as created, regardless of fonts, software, and",
 						"operating systems. Fonts, and graphics are not lost due to platform, software, and",
 						"version incompatibilities.",
-						"• The  free  Acrobat  Reader  is  easy  to  download  and  can  be  freely  distributed  by",
+						"• The free Acrobat Reader is easy to download and can be freely distributed by",
 						"anyone.",
-						"• Compact   PDF   files   are   smaller   than   their   source   files   and   download   a",
+						"• Compact PDF files are smaller than their source files and download a",
 						"page at a time for fast display on the Web."
 					];
 					expect(text.join("\n")).toBe(content.join("\n"));
@@ -206,7 +206,7 @@ describe("PDFExtract.tools", () => {
 				try {
 					expect(rows.length).toBe(1);
 					const text = rows.map((row) => row.join("")).join("\n");
-					expect(text).toBe("Hello I’m an encrypted pdf ");
+					expect(text).toBe("Hello I’m an encrypted pdf");
 					done();
 				} catch (error) {
 					done(error);
--- update_pdfjs.sh
+++ update_pdfjs.sh
@@ -3,10 +3,10 @@ cd pdfjs-temp
 npm install
 npm install gulp
 ./node_modules/.bin/gulp dist-pre
-#cp ./build/dist/build/pdf.js ../lib/pdfjs/pdf.js
-#cp ./build/dist/build/pdf.worker.js ../lib/pdfjs/pdf.worker.js
-node -e 'var fs=require("fs");fs.writeFileSync("../lib/pdfjs/pdf.js",fs.readFileSync("./build/generic-es5/build/pdf.js").toString().replace("var verbosity = VerbosityLevel.WARNINGS","var verbosity = VerbosityLevel.ERRORS"));process.exit()'
-node -e 'var fs=require("fs");fs.writeFileSync("../lib/pdfjs/pdf.worker.js",fs.readFileSync("./build/generic-es5/build/pdf.worker.js").toString().replace("var verbosity = VerbosityLevel.WARNINGS","var verbosity = VerbosityLevel.ERRORS"));process.exit()'
+#cp ./build/generic-legacy/build/pdf.js ../lib/pdfjs/pdf.js
+#cp ./build/generic-legacy/build/pdf.worker.js ../lib/pdfjs/pdf.worker.js
+node -e 'var fs=require("fs");fs.writeFileSync("../lib/pdfjs/pdf.js",fs.readFileSync("./build/generic-legacy/build/pdf.js").toString().replace("var verbosity = VerbosityLevel.WARNINGS","var verbosity = VerbosityLevel.ERRORS"));process.exit()'
+node -e 'var fs=require("fs");fs.writeFileSync("../lib/pdfjs/pdf.worker.js",fs.readFileSync("./build/generic-legacy/build/pdf.worker.js").toString().replace("var verbosity = VerbosityLevel.WARNINGS","var verbosity = VerbosityLevel.ERRORS"));process.exit()'
 cp ./build/version.json ../lib/pdfjs/version.json
 cp ./LICENSE ../lib/pdfjs/LICENSE
 echo Used version
