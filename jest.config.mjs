a4c2e71eedd56d4f4f609a35e99a45fc37d64bc4
Shaun Fitzgibbon <shaun@fitzindustries.com>
2024-04-18 17:13:44 +0200

    get tests working with esm pdfExtract

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,7 +1,5 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -69,14 +67,7 @@ module.exports = {
   // ],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "json",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "node"
-  // ],
+  moduleFileExtensions: ["mjs", "cjs", "js", "json", "node"],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
@@ -141,10 +132,7 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: ["<rootDir>/test/((*.)+)?test.(m)?js"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
@@ -186,3 +174,5 @@ module.exports = {
   // Whether to use watchman for file crawling
   // watchman: true,
 };
+
+export default config;
--- test/test.js
+++ test/test.js
@@ -1,234 +1,262 @@
-const path = require("path");
-const fs = require("fs");
-const PDFExtract = require("../lib").PDFExtract;
+import { resolve as _resolve, dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { readFileSync, readFile } from "fs";
+import { PDFExtract } from "../lib/index.mjs";
 
-const pdfDirectory = path.resolve(__dirname, "../example/");
-const sampleFile = path.join(pdfDirectory, "example.pdf");
-const sampleOutput = JSON.parse(fs.readFileSync(path.join(pdfDirectory, "example-output.json")).toString());
-const sampleEncryptedFile = path.join(pdfDirectory, "encrypted.pdf");
-const sampleEncryptedOutput = JSON.parse(fs.readFileSync(path.join(pdfDirectory, "encrypted-output.json")).toString());
-const sampleCmapFile = path.join(pdfDirectory, "example-cmap.pdf");
-const sampleCmapOutput = JSON.parse(fs.readFileSync(path.join(pdfDirectory, "example-cmap-output.json")).toString());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const pdfDirectory = _resolve(__dirname, "../example/");
+
+const sampleFile = join(pdfDirectory, "example.pdf");
+const sampleOutput = JSON.parse(
+  readFileSync(join(pdfDirectory, "example-output.json")).toString(),
+);
+const sampleEncryptedFile = join(pdfDirectory, "encrypted.pdf");
+const sampleEncryptedOutput = JSON.parse(
+  readFileSync(join(pdfDirectory, "encrypted-output.json")).toString(),
+);
+const sampleCmapFile = join(pdfDirectory, "example-cmap.pdf");
+const sampleCmapOutput = JSON.parse(
+  readFileSync(join(pdfDirectory, "example-cmap-output.json")).toString(),
+);
 
 function readFileAsync(filename) {
-	return new Promise((resolve, reject) => {
-		fs.readFile(filename, (err, data) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(data);
-			}
-		});
-	});
+  return new Promise((resolve, reject) => {
+    readFile(filename, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
 }
 
 function deepEqualPages(a, b, ignoreProperties) {
-	// fontNames may be generated ids
-	const cloneA = a.map(item => {
-		return {
-			...item, content: item.content.map(entry => {
-				return {...entry, fontName: undefined}
-			})
-		};
-	});
-	const cloneB = a.map(item => {
-		return {
-			...item, content: item.content.map(entry => {
-				return {...entry, fontName: undefined}
-			})
-		};
-	});
-	expect(cloneA).toEqual(cloneB);
+  // fontNames may be generated ids
+  const cloneA = a.map((item) => {
+    return {
+      ...item,
+      content: item.content.map((entry) => {
+        return { ...entry, fontName: undefined };
+      }),
+    };
+  });
+  const cloneB = a.map((item) => {
+    return {
+      ...item,
+      content: item.content.map((entry) => {
+        return { ...entry, fontName: undefined };
+      }),
+    };
+  });
+  expect(cloneA).toEqual(cloneB);
 }
 
 describe("PDFExtract", () => {
+  describe("#extractBuffer()", () => {
+    it("should extract pdf buffer without error", (done) => {
+      const extract = new PDFExtract();
+      const buffer = readFileSync(sampleFile);
+      extract.extractBuffer(buffer, {}, (err) => {
+        if (err) done(err);
+        else done();
+      });
+    });
 
-	describe("#extractBuffer()", () => {
-		it("should extract pdf buffer without error", (done) => {
-			const extract = new PDFExtract();
-			const buffer = fs.readFileSync(sampleFile);
-			extract.extractBuffer(buffer, {}, (err) => {
-				if (err) done(err);
-				else done();
-			});
-		});
-		it("should extract pdf buffer with right data", (done) => {
-			const extract = new PDFExtract();
-			const buffer = fs.readFileSync(sampleFile);
-			extract.extractBuffer(buffer, {}, (err, data) => {
-				if (err) return done(err);
-				try {
-					expect(data.meta).toEqual(sampleOutput.meta);
-					deepEqualPages(data.pages, sampleOutput.pages, ["fontName"]);
-					done();
-				} catch (error) {
-					done(error);
-				}
-			});
-		});
-		it("should async extract pdf buffer with right data", async () => {
-			const extract = new PDFExtract();
-			const buffer = await readFileAsync(sampleFile);
-			const data = await extract.extractBuffer(buffer, {});
-			expect(data.meta).toEqual(sampleOutput.meta);
-			deepEqualPages(data.pages, sampleOutput.pages, ["fontName"]);
-		});
-		it("should extract encrypted pdf buffer without error", (done) => {
-			const extract = new PDFExtract();
-			const buffer = fs.readFileSync(sampleEncryptedFile);
-			extract.extractBuffer(buffer, {password: "password"}, (err) => {
-				if (err) done(err);
-				else done();
-			});
-		});
-		it("should extract encrypted pdf buffer with right data", (done) => {
-			const extract = new PDFExtract();
-			const buffer = fs.readFileSync(sampleEncryptedFile);
-			extract.extractBuffer(buffer, {password: "password"}, (err, data) => {
-				if (err) return done(err);
-				try {
-					expect(data.meta).toEqual(sampleEncryptedOutput.meta);
-					deepEqualPages(data.pages, sampleEncryptedOutput.pages, ["fontName"]);
-					done();
-				} catch (error) {
-					done(error);
-				}
-			});
-		});
-		it("should fail with wrong password on encrypted pdf buffer with error", (done) => {
-			const extract = new PDFExtract();
-			const buffer = fs.readFileSync(sampleEncryptedFile);
-			extract.extractBuffer(buffer, {password: "wrong"}, (err) => {
-				try {
-					expect(err.name).toBe("PasswordException");
-					done();
-				} catch (error) {
-					done(error);
-				}
-			});
-		});
-	});
-	describe("#extract()", () => {
-		it("should load and extract pdf without error", (done) => {
-			const extract = new PDFExtract();
-			extract.extract(sampleFile, {}, (err) => {
-				if (err) done(err);
-				else done();
-			});
-		});
-		it("should async load and extract pdf with the right data", async () => {
-			const extract = new PDFExtract();
-			const data = await extract.extract(sampleFile, {});
-			//excludingEvery("fontName")
-			expect(data.meta).toEqual(sampleOutput.meta);
-			deepEqualPages(data.pages, sampleOutput.pages, ["fontName"]);
-		});
-		it("should load and extract encrypted pdf without error", (done) => {
-			const extract = new PDFExtract();
-			extract.extract(sampleEncryptedFile, {password: "password"}, (err) => {
-				if (err) done(err);
-				else done();
-			});
-		});
-		it("should load and fail with wrong password on encrypted pdf with error", (done) => {
-			const extract = new PDFExtract();
-			extract.extract(sampleEncryptedFile, {password: "wrong"}, (err) => {
-				try {
-					expect(err.name).toBe("PasswordException");
-					done();
-				} catch (error) {
-					done(error);
-				}
-			});
-		});
-		it("should load and extract cmap-pdf without error", (done) => {
-			const extract = new PDFExtract();
-			extract.extract(sampleCmapFile, {}, (err) => {
-				if (err) done(err);
-				else done();
-			});
-		});
-		it("should load and extract cmap-pdf with the right data", async () => {
-			const extract = new PDFExtract();
-			const data = await extract.extract(sampleCmapFile);
-			expect(data.meta).toEqual(sampleCmapOutput.meta);
-			deepEqualPages(data.pages, sampleCmapOutput.pages, ["fontName"]);
-		});
-	});
+    it("should extract pdf buffer with right data", (done) => {
+      const extract = new PDFExtract();
+      const buffer = readFileSync(sampleFile);
+      extract.extractBuffer(buffer, {}, (err, data) => {
+        if (err) return done(err);
+        try {
+          expect(data.meta).toEqual(sampleOutput.meta);
+          deepEqualPages(data.pages, sampleOutput.pages, ["fontName"]);
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+    });
 
+    it("should async extract pdf buffer with right data", async () => {
+      const extract = new PDFExtract();
+      const buffer = await readFileAsync(sampleFile);
+      const data = await extract.extractBuffer(buffer, {});
+      expect(data.meta).toEqual(sampleOutput.meta);
+      deepEqualPages(data.pages, sampleOutput.pages, ["fontName"]);
+    });
+
+    it("should extract encrypted pdf buffer without error", (done) => {
+      const extract = new PDFExtract();
+      const buffer = readFileSync(sampleEncryptedFile);
+      extract.extractBuffer(buffer, { password: "password" }, (err) => {
+        if (err) done(err);
+        else done();
+      });
+    });
+
+    it("should extract encrypted pdf buffer with right data", (done) => {
+      const extract = new PDFExtract();
+      const buffer = readFileSync(sampleEncryptedFile);
+      extract.extractBuffer(buffer, { password: "password" }, (err, data) => {
+        if (err) return done(err);
+        try {
+          expect(data.meta).toEqual(sampleEncryptedOutput.meta);
+          deepEqualPages(data.pages, sampleEncryptedOutput.pages, ["fontName"]);
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+    });
+
+    it("should fail with wrong password on encrypted pdf buffer with error", (done) => {
+      const extract = new PDFExtract();
+      const buffer = readFileSync(sampleEncryptedFile);
+      extract.extractBuffer(buffer, { password: "wrong" }, (err) => {
+        try {
+          expect(err.name).toBe("PasswordException");
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+    });
+  });
+
+  describe("#extract()", () => {
+    it("should load and extract pdf without error", (done) => {
+      const extract = new PDFExtract();
+      extract.extract(sampleFile, {}, (err) => {
+        if (err) done(err);
+        else done();
+      });
+    });
+
+    it("should async load and extract pdf with the right data", async () => {
+      const extract = new PDFExtract();
+      const data = await extract.extract(sampleFile, {});
+      //excludingEvery("fontName")
+      expect(data.meta).toEqual(sampleOutput.meta);
+      deepEqualPages(data.pages, sampleOutput.pages, ["fontName"]);
+    });
+
+    it("should load and extract encrypted pdf without error", (done) => {
+      const extract = new PDFExtract();
+      extract.extract(sampleEncryptedFile, { password: "password" }, (err) => {
+        if (err) done(err);
+        else done();
+      });
+    });
+
+    it("should load and fail with wrong password on encrypted pdf with error", (done) => {
+      const extract = new PDFExtract();
+      extract.extract(sampleEncryptedFile, { password: "wrong" }, (err) => {
+        try {
+          expect(err.name).toBe("PasswordException");
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+    });
+
+    it("should load and extract cmap-pdf without error", (done) => {
+      const extract = new PDFExtract();
+      extract.extract(sampleCmapFile, {}, (err) => {
+        if (err) done(err);
+        else done();
+      });
+    });
+
+    it("should load and extract cmap-pdf with the right data", async () => {
+      const extract = new PDFExtract();
+      const data = await extract.extract(sampleCmapFile);
+      expect(data.meta).toEqual(sampleCmapOutput.meta);
+      deepEqualPages(data.pages, sampleCmapOutput.pages, ["fontName"]);
+    });
+  });
 });
 
 describe("PDFExtract.tools", () => {
-	describe("pageToLines", () => {
-		it("should return the correct example lines", (done) => {
-			const extract = new PDFExtract();
-			extract.extract(sampleFile, {}, (err, data) => {
-				if (err) return done(err);
-				const page = data.pages[0];
-				const lines = PDFExtract.utils.pageToLines(page, 2);
-				const rows = PDFExtract.utils.extractTextRows(lines);
-				try {
-					expect(rows.length).toBe(17);
-					const text = rows.map((row) => row.join(""));
-					const content = [
-						"Adobe Acrobat PDF Files",
-						"Adobe® Portable Document Format (PDF) is a universal file format that preserves all",
-						"of the fonts, formatting, colours and graphics of any source document, regardless of",
-						"the application and platform used to create it.",
-						"Adobe PDF is an ideal format for electronic document distribution as it overcomes the",
-						"problems commonly encountered with electronic file sharing.",
-						"• Anyone, anywhere can open a PDF file. All you need is the free Adobe Acrobat",
-						"Reader. Recipients of other file formats sometimes can't open files because they",
-						"don't have the applications used to create the documents.",
-						"• PDF files always print correctly on any printing device.",
-						"• PDF files always display exactly as created, regardless of fonts, software, and",
-						"operating systems. Fonts, and graphics are not lost due to platform, software, and",
-						"version incompatibilities.",
-						"• The free Acrobat Reader is easy to download and can be freely distributed by",
-						"anyone.",
-						"• Compact PDF files are smaller than their source files and download a",
-						"page at a time for fast display on the Web."
-					];
-					expect(text.join("\n")).toBe(content.join("\n"));
-					done();
-				} catch (error) {
-					done(error);
-				}
-			});
-		});
-		it("should return the correct encrypted example lines", (done) => {
-			const extract = new PDFExtract();
-			extract.extract(sampleEncryptedFile, {password: "password"}, (err, data) => {
-				if (err) return done(err);
-				const page = data.pages[0];
-				const lines = PDFExtract.utils.pageToLines(page, 2);
-				const rows = PDFExtract.utils.extractTextRows(lines);
-				try {
-					expect(rows.length).toBe(1);
-					const text = rows.map((row) => row.join("")).join("\n");
-					expect(text).toBe("Hello I’m an encrypted pdf");
-					done();
-				} catch (error) {
-					done(error);
-				}
-			});
-		});
-		it("should return the correct cmap example lines", (done) => {
-			const extract = new PDFExtract();
-			extract.extract(sampleCmapFile, {}, (err, data) => {
-				if (err) return done(err);
-				const page = data.pages[0];
-				const lines = PDFExtract.utils.pageToLines(page, 2);
-				const rows = PDFExtract.utils.extractTextRows(lines);
-				try {
-					expect(rows.length).toBe(1);
-					const text = rows.map((row) => row.join("")).join("\n");
-					expect(text).toBe("我们都是黑体字");
-					done();
-				} catch (error) {
-					done(error);
-				}
-			});
-		});
-	});
+  describe("pageToLines", () => {
+    it("should return the correct example lines", (done) => {
+      const extract = new PDFExtract();
+      extract.extract(sampleFile, {}, (err, data) => {
+        if (err) return done(err);
+        const page = data.pages[0];
+        const lines = PDFExtract.utils.pageToLines(page, 2);
+        const rows = PDFExtract.utils.extractTextRows(lines);
+        try {
+          expect(rows.length).toBe(17);
+          const text = rows.map((row) => row.join(""));
+          const content = [
+            "Adobe Acrobat PDF Files",
+            "Adobe® Portable Document Format (PDF) is a universal file format that preserves all",
+            "of the fonts, formatting, colours and graphics of any source document, regardless of",
+            "the application and platform used to create it.",
+            "Adobe PDF is an ideal format for electronic document distribution as it overcomes the",
+            "problems commonly encountered with electronic file sharing.",
+            "• Anyone, anywhere can open a PDF file. All you need is the free Adobe Acrobat",
+            "Reader. Recipients of other file formats sometimes can't open files because they",
+            "don't have the applications used to create the documents.",
+            "• PDF files always print correctly on any printing device.",
+            "• PDF files always display exactly as created, regardless of fonts, software, and",
+            "operating systems. Fonts, and graphics are not lost due to platform, software, and",
+            "version incompatibilities.",
+            "• The free Acrobat Reader is easy to download and can be freely distributed by",
+            "anyone.",
+            "• Compact PDF files are smaller than their source files and download a",
+            "page at a time for fast display on the Web.",
+          ];
+          expect(text.join("\n")).toBe(content.join("\n"));
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+    });
+
+    it("should return the correct encrypted example lines", (done) => {
+      const extract = new PDFExtract();
+      extract.extract(
+        sampleEncryptedFile,
+        { password: "password" },
+        (err, data) => {
+          if (err) return done(err);
+          const page = data.pages[0];
+          const lines = PDFExtract.utils.pageToLines(page, 2);
+          const rows = PDFExtract.utils.extractTextRows(lines);
+          try {
+            expect(rows.length).toBe(1);
+            const text = rows.map((row) => row.join("")).join("\n");
+            expect(text).toBe("Hello I’m an encrypted pdf");
+            done();
+          } catch (error) {
+            done(error);
+          }
+        },
+      );
+    });
+
+    it("should return the correct cmap example lines", (done) => {
+      const extract = new PDFExtract();
+      extract.extract(sampleCmapFile, {}, (err, data) => {
+        if (err) return done(err);
+        const page = data.pages[0];
+        const lines = PDFExtract.utils.pageToLines(page, 2);
+        const rows = PDFExtract.utils.extractTextRows(lines);
+        try {
+          expect(rows.length).toBe(1);
+          const text = rows.map((row) => row.join("")).join("\n");
+          expect(text).toBe("我们都是黑体字");
+          done();
+        } catch (error) {
+          done(error);
+        }
+      });
+    });
+  });
 });
