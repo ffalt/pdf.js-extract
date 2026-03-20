d3daa798acdc582ba52d0fc30558315e86fb67d6
ffalt <ffalty@gmail.com>
2026-03-20 14:28:48 +0100

    cleanup examples and tests, add attachment.pdf

--- test/test.mjs
+++ test/test.mjs
@@ -12,19 +12,29 @@ const testCases = [
 	{
 		name: "example",
 		file: "example.pdf",
-		outputFile: "example-output.json",
+		outputFile: "example.json",
+		textFile: "example.txt",
 		password: undefined
 	},
 	{
 		name: "encrypted",
 		file: "encrypted.pdf",
-		outputFile: "encrypted-output.json",
+		outputFile: "encrypted.json",
+		textFile: "encrypted.txt",
 		password: "password"
 	},
 	{
 		name: "cmap",
-		file: "example-cmap.pdf",
-		outputFile: "example-cmap-output.json",
+		file: "cmap.pdf",
+		outputFile: "cmap.json",
+		textFile: "cmap.txt",
+		password: undefined
+	},
+	{
+		name: "attachment",
+		file: "attachment.pdf",
+		outputFile: "attachment.json",
+		textFile: "attachment.txt",
 		password: undefined
 	}
 ];
@@ -32,7 +42,8 @@ const testCases = [
 const loadedTestCases = testCases.map(tc => ({
 	...tc,
 	filePath: path.join(pdfDirectory, tc.file),
-	output: JSON.parse(fs.readFileSync(path.join(pdfDirectory, tc.outputFile)).toString())
+	output: JSON.parse(fs.readFileSync(path.join(pdfDirectory, tc.outputFile)).toString()),
+	expectedText: fs.readFileSync(path.join(pdfDirectory, tc.textFile)).toString().trim().split('\n')
 }));
 
 function readFileAsync(filename) {
@@ -156,59 +167,16 @@ describe("PDFExtract", () => {
 
 describe("PDFExtract.tools", () => {
 	describe("pageToLines", () => {
-		const pageToLinesTestCases = [
-			{
-				name: "example",
-				file: "example.pdf",
-				password: undefined,
-				expectedLines: 17,
-				expectedText: [
-					"Adobe Acrobat PDF Files",
-					"Adobe® Portable Document Format (PDF) is a universal file format that preserves all",
-					"of the fonts, formatting, colours and graphics of any source document, regardless of",
-					"the application and platform used to create it.",
-					"Adobe PDF is an ideal format for electronic document distribution as it overcomes the",
-					"problems commonly encountered with electronic file sharing.",
-					"• Anyone, anywhere can open a PDF file. All you need is the free Adobe Acrobat",
-					"Reader. Recipients of other file formats sometimes can't open files because they",
-					"don't have the applications used to create the documents.",
-					"• PDF files always print correctly on any printing device.",
-					"• PDF files always display exactly as created, regardless of fonts, software, and",
-					"operating systems. Fonts, and graphics are not lost due to platform, software, and",
-					"version incompatibilities.",
-					"• The free Acrobat Reader is easy to download and can be freely distributed by",
-					"anyone.",
-					"• Compact PDF files are smaller than their source files and download a",
-					"page at a time for fast display on the Web."
-				]
-			},
-			{
-				name: "encrypted",
-				file: "encrypted.pdf",
-				password: "password",
-				expectedLines: 1,
-				expectedText: ["Hello I'm an encrypted pdf"]
-			},
-			{
-				name: "cmap",
-				file: "example-cmap.pdf",
-				password: undefined,
-				expectedLines: 1,
-				expectedText: ["我们都是黑体字"]
-			}
-		];
-
-		it.each(pageToLinesTestCases)("should return the correct $name example lines", (testCase, done) => {
+		it.each(loadedTestCases)("should return the correct $name example lines", (testCase, done) => {
 			const extract = new PDFExtract();
-			const filePath = path.join(pdfDirectory, testCase.file);
 			const options = testCase.password ? { password: testCase.password } : {};
-			extract.extract(filePath, options, (err, data) => {
+			extract.extract(testCase.filePath, options, (err, data) => {
 				if (err) return done(err);
 				const page = data.pages[0];
 				const lines = PDFExtract.utils.pageToLines(page, 2);
 				const rows = PDFExtract.utils.extractTextRows(lines);
 				try {
-					expect(rows.length).toBe(testCase.expectedLines);
+					expect(rows.length).toBe(testCase.expectedText.length);
 					const text = rows.map((row) => row.join(""));
 					expect(text.join("\n")).toBe(testCase.expectedText.join("\n"));
 					done();
@@ -219,4 +187,3 @@ describe("PDFExtract.tools", () => {
 		});
 	});
 });
-
